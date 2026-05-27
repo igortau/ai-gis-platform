f0f381483e8e788cd0d47921e55cebf17b84668f
igortau <igorsup@gmail.com>
2026-05-27 19:21:28 +0300

    Update spatial_queries.sql

--- examples/spatial_queries.sql
+++ examples/spatial_queries.sql
@@ -1,1 +1,12 @@
+-- Count features
+SELECT COUNT(*)
+FROM abkhazia.city;
 
+-- Geometry types
+SELECT DISTINCT ST_GeometryType(geom)
+FROM abkhazia.city;
+
+-- Invalid geometries
+SELECT COUNT(*)
+FROM abkhazia.city
+WHERE NOT ST_IsValid(geom);
